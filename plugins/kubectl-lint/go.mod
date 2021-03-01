a962c67eaa8fb22cb70f1fed0f85259ff4621155
sulabh chaturvedi <sulabhchaturvedi08@gmail.com>
2021-03-01 13:06:11 +0000

    asthetics and solve nil pointer ref due to empty *corba.command

--- plugins/kubectl-lint/go.mod
+++ plugins/kubectl-lint/go.mod
@@ -1,13 +1,13 @@
 module sigs.k8s.io/cli-experimental/plugins/kubectl-lint
 
-go 1.15
+go 1.16
 
 require (
-	github.com/spf13/cobra v1.1.1
-	github.com/stretchr/testify v1.6.1
+	github.com/spf13/cobra v1.1.3
+	github.com/stretchr/testify v1.7.0
 	k8s.io/apimachinery v0.20.4
 	k8s.io/cli-runtime v0.20.4
 	k8s.io/client-go v0.20.4
 	k8s.io/kubectl v0.20.4
-	sigs.k8s.io/cli-utils v0.23.3
+	sigs.k8s.io/cli-utils v0.23.5
 )
--- plugins/kubectl-lint/main.go
+++ plugins/kubectl-lint/main.go
@@ -4,17 +4,12 @@
 package main
 
 import (
-	"flag"
 	"os"
 
 	"github.com/spf13/cobra"
-	"k8s.io/apimachinery/pkg/api/meta"
 	"k8s.io/cli-runtime/pkg/genericclioptions"
-	"k8s.io/cli-runtime/pkg/resource"
-	"k8s.io/client-go/dynamic"
 	cmdutil "k8s.io/kubectl/pkg/cmd/util"
 	"k8s.io/kubectl/pkg/util/i18n"
-	"k8s.io/kubectl/pkg/util/logs"
 	"sigs.k8s.io/cli-experimental/plugins/kubectl-lint/pkglint"
 	"sigs.k8s.io/cli-utils/pkg/errors"
 	// This is here rather than in the libraries because of
@@ -23,28 +18,14 @@ import (
 	_ "k8s.io/client-go/plugin/pkg/client/auth"
 )
 
-// LintOptions options for lint subcommand
-type LintOptions struct {
-	FileNameOpts resource.FilenameOptions
-
-	RecordFlags *genericclioptions.RecordFlags
-	Recorder    genericclioptions.Recorder
-
-	DynamicClient dynamic.Interface
-	Mapper        meta.RESTMapper
-	Result        *resource.Result
-
-	genericclioptions.IOStreams
-}
-
-// NewLintOptions constructor to type LintOptions
-func NewLintOptions(streams genericclioptions.IOStreams) *LintOptions {
-	return &LintOptions{
-		FileNameOpts: resource.FilenameOptions{},
-		RecordFlags:  genericclioptions.NewRecordFlags(),
-		Recorder:     genericclioptions.NoopRecorder{},
-		IOStreams:    streams,
-	}
+var cmd = &cobra.Command{
+	Use:   "lint [-f FILENAME] [-k DIRECTORY]",
+	Short: i18n.T("Lint resource configuration files."),
+	Long:  i18n.T(`
+Look for common issues with resource configuration.  Emit an error message if kubernetes best practices not followed and exit non-0.
+`),
+	SilenceErrors: true,
+	SilenceUsage:  true,
 }
 
 func main() {
@@ -54,38 +35,16 @@ func main() {
 		ErrOut: os.Stderr,
 	}
 
-	o := NewLintOptions(ioStreams)
-	var f cmdutil.Factory
-	lintCmd := &pkglint.Cmd{
-		Factory:  f,
-		FileNameOptions: &o.FileNameOpts,
-		IOStreams:       ioStreams,
-	}
+	flags := cmd.PersistentFlags()
+	kubeConfigFlags := genericclioptions.NewConfigFlags(true).WithDeprecatedPasswordFlag()
+	kubeConfigFlags.AddFlags(flags)
+	matchVersionKubeConfigFlags := cmdutil.NewMatchVersionFlags(kubeConfigFlags)
 
-	cmd := &cobra.Command{
-		Use:                   "kubectl lint [-f FILENAME] [-k DIRECTORY]",
-		DisableFlagsInUseLine: true,
-		Short:                 i18n.T("Lint resource configuration files."),
-		Long: i18n.T(`
-Look for common issues with resource configuration.  Emit an error message if kubernetes best practices not followed and exit non-0.
-`),
-		Example: i18n.T(`#kubectl lint example.yaml and exit non-0 if issues found.
-kubectl lint -f example.yaml
-`),
-		RunE: func(cmd *cobra.Command, args []string) error {
-			return lintCmd.Run()
-		},
-	}
-
-	// configure kubectl dependencies and flags
-	o.RecordFlags.AddFlags(cmd)
-	cmdutil.AddFilenameOptionFlags(cmd, &o.FileNameOpts, "full path to Kubernetes manifests (.yaml) to lint")
-	cmd.PersistentFlags().AddGoFlagSet(flag.CommandLine)
-
-	logs.InitLogs()
-	defer logs.FlushLogs()
+	f := cmdutil.NewFactory(matchVersionKubeConfigFlags)
+	
+	cmd.AddCommand(pkglint.NewCmdLint(f, ioStreams))
 
 	if err := cmd.Execute(); err != nil {
-		errors.CheckErr(cmd.ErrOrStderr(), err, "klint")
+		errors.CheckErr(cmd.ErrOrStderr(), err, "kubectl-lint")
 	}
 }
